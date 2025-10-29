c4f6fa2e162e80e10d04da7a74378311d95f08be
Likhith Kolayari <likhith@regentmarkets.com>
2025-10-29 20:40:15 +0530

    added styles for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import { ThemeToggle } from "./components";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import './App.css'
 
 function App() {
@@ -14,13 +15,26 @@ function App() {
 
   return (
     <div className="bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
-      <header className="flex justify-between items-center p-4">
-        <h1 className="text-xl font-bold">Likhith Kolayari</h1>
-        <ThemeToggle theme={theme} setTheme={setTheme} />
-      </header>
-    <main className="space-y-12">
-      <Home/>
-    </main>
+      <TabGroup>
+        <header className="sticky top-0 z-10 flex w-full justify-center px-4 py-4 bg-white dark:bg-gray-900 shadow-md">
+          <TabList className="flex gap-4">
+            <Tab key="home" className="tab-item">HOME</Tab>
+            <Tab key="about_me" className="tab-item">ABOUT ME</Tab>
+            <Tab key="my_journey" className="tab-item">MY JOURNEY</Tab>
+            <Tab key="contact" className="tab-item">CONTACT</Tab>
+            <ThemeToggle theme={theme} setTheme={setTheme} />
+          </TabList>
+        </header>
+        
+        <main className="container mx-auto px-4 py-8 w-full">
+          <TabPanels>
+            <TabPanel><Home/></TabPanel>
+            <TabPanel>About me section</TabPanel>
+            <TabPanel>My journey section</TabPanel>
+            <TabPanel>Contact section</TabPanel>
+          </TabPanels>
+        </main>
+      </TabGroup>
     </div>
   )
 }
