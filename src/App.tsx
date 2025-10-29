be745eb530b56e50f07cb27b9b4222aefc49c3ab
Likhith Kolayari <likhith@regentmarkets.com>
2025-10-29 23:29:53 +0530

    added workflow to automateautomated deployment via workflow

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,12 @@ function App() {
     <div className="bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
       <TabGroup>
         <header className="sticky top-0 z-10 flex w-full justify-center px-4 py-4 bg-white dark:bg-gray-900 shadow-md">
-          <TabList className="flex gap-4">
+          <TabList className="flex gap-4 items-center">
             <Tab key="home" className="tab-item">HOME</Tab>
             <Tab key="about_me" className="tab-item">ABOUT ME</Tab>
             <Tab key="my_journey" className="tab-item">MY JOURNEY</Tab>
             <Tab key="contact" className="tab-item">CONTACT</Tab>
+            <div className="h-6 w-px bg-gray-700 dark:bg-white mx-2"></div>
             <ThemeToggle theme={theme} setTheme={setTheme} />
           </TabList>
         </header>
@@ -29,9 +30,9 @@ function App() {
         <main className="container mx-auto px-4 py-8 w-full">
           <TabPanels>
             <TabPanel><Home/></TabPanel>
-            <TabPanel>About me section</TabPanel>
-            <TabPanel>My journey section</TabPanel>
-            <TabPanel>Contact section</TabPanel>
+            <TabPanel>Placeholder for About me section</TabPanel>
+            <TabPanel>Placeholder for My journey section</TabPanel>
+            <TabPanel>Placeholder for Contact section</TabPanel>
           </TabPanels>
         </main>
       </TabGroup>
